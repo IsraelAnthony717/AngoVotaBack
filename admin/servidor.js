563ad5ae689ef1b539a2ac9a6ecca1e18d983747
Israel Anthony <seu-email@gmail.com>
2026-04-05 23:24:56 +0100

    Atualização_3

--- admin/servidor.js
+++ admin/servidor.js
@@ -1,9 +1,7 @@
 const { server } = require('./app');
 
 const db = require('./models');
-db.sequelize.sync({ alter: false })  // cuidado: alter: true modifica colunas existentes
-  .then(() => console.log('✅ Tabelas sincronizadas'))
-  .catch(err => console.error('❌ Erro ao sincronizar:', err));
+console.log('Modelos carregados:', Object.keys(db));
 
 server.listen(process.env.PORT || 3003, "0.0.0.0", ()=>{
     console.log(`Servidor rodando na porta ${process.env.PORT}`);
