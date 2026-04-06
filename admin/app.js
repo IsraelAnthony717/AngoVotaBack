ae54301425700f6999d59ce745e26902bf1842d6
Israel Anthony <seu-email@gmail.com>
2026-04-06 21:34:38 +0100

    Atualização_10.09

--- admin/app.js
+++ admin/app.js
@@ -38,8 +38,8 @@ app.use(cors({
 }));
 
 // Middlewares padrão
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Configuração da sessão (essencial para cross-origin)
 app.use(session({
